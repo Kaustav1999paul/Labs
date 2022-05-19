3473f3793439e133454ed7a41d9fe0d220c6e759
Kaustav1999paul <kaustavpaul347@gmail.com>
2022-05-19 09:34:44 +0530

    small

--- Wap/5/index.js
+++ Wap/5/index.js
@@ -15,10 +15,6 @@ const data = [
 
   ];
 
-  const width = 1000;
-  const height = 500;
-  const margin = { top: 50, bottom: 50, left: 50, right: 50 };
-
   const svg = d3.select('#d3-container')
     .append('svg')
     .attr('width', 1000)
