bcb409c8fa8ba688d0778beaf770fb9c1dec25a8
Kaustav1999paul <kaustavpaul347@gmail.com>
2022-05-19 09:14:36 +0530

    d3

--- Wap/5/index.js
+++ Wap/5/index.js
@@ -21,38 +21,37 @@ const data = [
 
   const svg = d3.select('#d3-container')
     .append('svg')
-    .attr('width', width - margin.left - margin.right)
-    .attr('height', height - margin.top - margin.bottom)
-    .attr("viewBox", [0, 0, width, height]);
+    .attr('width', 1000)
+    .attr('height', 500);
 
   const x = d3.scaleBand()
     .domain(d3.range(data.length))
-    .range([margin.left, width - margin.right])
+    .range([50,1000])
     .padding(0.3)
 
   const y = d3.scaleLinear()
     .domain([0, 120])
-    .range([height - margin.bottom, margin.top])
+    .range([450, 50])
 
   svg.append("g")
-    .attr("fill", 'orange')
     .selectAll("rect")
     .data(data)
     .join("rect")
     .attr("x", (d, i) => x(i))
-    .attr("y", d => y(d.score))
-    .attr('title', (d) => d.score)
-    .attr("class", "rect")
+    .attr("y", (d) => y(d.score))
     .attr("height", d => y(0) - y(d.score))
-    .attr("width", x.bandwidth());
+    .attr("width", x.bandwidth())
+    .attr("fill", 'orange');
 
   function yAxis(g) {
-    g.attr("transform", `translate(${margin.left}, 0)`).call(d3.axisLeft(y).ticks(null, data.format)).attr("font-size", '20px')
+    g.attr("transform", `translate(50, 0)`)
+    .call(d3.axisLeft(y))
+    .attr("font-size", '20px')
   }
 
   function xAxis(g) {
-    g.attr("transform", `translate(0,${height - margin.bottom})`)
-      .call(d3.axisBottom(x).tickFormat(i => data[i].name))
+    g.attr("transform", `translate(0,450)`)
+      .call(d3.axisBottom(x))
       .attr("font-size", '20px')
   }
 
